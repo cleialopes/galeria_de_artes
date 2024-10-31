b0548fd2b52af6d71e910d273fc7904d276e16ef
cleialopes <cleialopes530@gmail.com>
2024-10-31 09:56:04 +0100

    primer commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+    function moveSlide(n) {
+    const slides = document.querySelector('.carousel-slide');
+    const totalSlides = slides.children.length;
+    slideIndex = (slideIndex + n + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(${-slideIndex * (100 / totalSlides)}%)`; // Ajusta el desplazamiento
+    }
+
+// Manejador de eventos para los botones "Agregar al Carrito"
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+button.addEventListener('click', () => {
+    const itemName = button.parentElement.querySelector('h3').innerText;
+    alert(`${itemName} ha sido agregado al carrito.`);
+});
+});
