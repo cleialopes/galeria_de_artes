6c4a801e1ffbfbdbd245da99fa5c32d095069f75
cleialopes <cleialopes530@gmail.com>
2024-11-04 21:47:52 +0100

    add

--- scripts.js
+++ scripts.js
@@ -16,10 +16,3 @@ button.addEventListener('click', () => {
     alert(`${itemName} ha sido agregado al carrito.`);
 });
 });
-
-// Mensajes de error y éxito personalizados
-window.REQUIRED_CODE_ERROR_MESSAGE = 'Elija un código de país';
-window.LOCALE = 'es';
-window.EMAIL_INVALID_MESSAGE = "La información que ha proporcionado no es válida. Compruebe el formato del campo e inténtelo de nuevo.";
-window.REQUIRED_ERROR_MESSAGE = "Este campo no puede quedarse vacío.";
-window.GENERIC_INVALID_MESSAGE = "La información que ha proporcionado no es válida. Compruebe el formato del campo e inténtelo de nuevo.";
